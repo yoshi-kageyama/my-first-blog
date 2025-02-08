488c401e5be9ea5e0d5dc4c8c11356536c0c3e4f
yoshi-kageyama <uc3h6z@bma.biglobe.ne.jp>
2025-02-08 22:10:40 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request,'blog/post_list.html',{})     # render は request 内容を使って blog/post_list.html  を組み立てを行う
