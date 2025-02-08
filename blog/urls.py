66a1a86162dc40c85fe8ba0c603ac898e38d74a8
yoshi-kageyama <uc3h6z@bma.biglobe.ne.jp>
2025-02-08 22:56:11 +0900

    #msg

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.post_list,name='post_list'),      # 一番最初がルートURLになる
+    path('',views.post_list,name='post_list'),  
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})     # render は request 内容を使って blog/post_list.html  を組み立てを行う
+    return render(request,'blog/post_list.html',{}) 
